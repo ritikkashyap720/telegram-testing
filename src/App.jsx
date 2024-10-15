59afe4738121c4044c7b3818b4b9f4ad36609ff7
Ritik Kashyap <ritikkashyap7404@gmail.com>
2024-10-15 14:57:46 +0530

    object to string

--- src/App.jsx
+++ src/App.jsx
@@ -7,31 +7,14 @@ const telegramObject = window.Telegram.WebApp;
 
 function App() {
   const [count, setCount] = useState(0)
-if(telegramObject){
-  console.log(telegramObject)
-}
+  if (telegramObject) {
+    console.log(telegramObject)
+  }
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <pre className='object'>
+      {JSON.stringify(telegramObject)}
+    </pre>
     </>
   )
 }
